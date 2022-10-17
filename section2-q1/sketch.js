af87639893ea5eba9e36fb490b2f488dfb31bdca
2130568h <aobthar@icloud.com>
2022-10-17 15:22:37 +0900

    Change section 2-q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,7 +1,16 @@
 // 小手調べ
 function setup(){
-  for(let i = 0; i < 10; i++){
-    ellipse(0, 0, 0);
+  noFill();
+
+  for(let i = 10; i >0; i--){
+   
     // BLANK[1]
+    if(5<i){
+      stroke(255,0,0);
+    }
+    else{
+      stroke(0,0,255);
+    }
+    ellipse(50,50,i*10-10);
   }
 }
