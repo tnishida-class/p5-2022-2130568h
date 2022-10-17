9cfcf9129834b186d963f57c22ccaac9a5878895
2130568h <aobthar@icloud.com>
2022-10-17 15:48:12 +0900

    Change 2-q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -4,18 +4,15 @@ function setup(){
   noFill();
 
   for(let i = 10; i >0; i--){
-   
-  for(let i = 0; i < 10; i++){ellipse(0, 0, 0);
 
     // BLANK[1]
     if(5<i){
       stroke(255,0,0);
     }
     else{
-      stroke(0,0,255);
-    ellipse(50,50,i*10-10);
+    stroke(0,0,255);
     }
+    ellipse(50,50,i*10-10);
   }
-}
 }
 
